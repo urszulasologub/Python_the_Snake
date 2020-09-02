34a1c7ad6eadc350495358ec7a7bfd56c0e9902b
urszulasologub <gold.ula4@gmail.com>
2020-09-02 21:36:41 +0200

    Add generating background based on screen size and blocks' size

--- interface/background.py
+++ interface/background.py
@@ -9,16 +9,18 @@ class Background:
 		self.height = screen_height
 		self.block_width = block_width
 		self.blocks_array = []
-		for i in range(0, self.width, self.block_width):
-			self.blocks_array.append(
-				Block(i, 0, self.block_width, self.block_height(), self.screen))
-			self.blocks_array.append(
-				Block(i, self.height - self.block_height(), self.block_width, self.block_height(), self.screen))
-		for i in range(self.block_height(), self.height - self.block_height(),self.block_height()):
+		lowest_y = 0
+		for i in range(self.block_height(), self.height - self.block_height(), self.block_height()):
 			self.blocks_array.append(
 				Block(0, i, self.block_width, self.block_height(), self.screen))
 			self.blocks_array.append(
 				Block(self.width - self.block_width, i, self.block_width, self.block_height(), self.screen))
+			lowest_y = i + self.block_height()
+		for i in range(0, self.width, self.block_width):
+			self.blocks_array.append(
+				Block(i, 0, self.block_width, self.block_height(), self.screen))
+			self.blocks_array.append(
+				Block(i, lowest_y, self.block_width, self.block_height(), self.screen))
 
 	def block_height(self):
 		return int((self.height / self.width) * self.block_width)
@@ -27,4 +29,6 @@ class Background:
 		for block in self.blocks_array:
 			block.draw()
 
+	def get_block_size(self):
+		return self.block_width, self.block_height()
 
